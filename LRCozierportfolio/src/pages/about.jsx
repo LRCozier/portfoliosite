ce2c36f6ce2022acf772d63f0d9baf313666fa8b
Luke Rudderham-Cozier <lrcozier@gmail.com>
2024-07-15 20:07:40 +0100

    added skills images and amended header.jsx

---
+++ LRCozierportfolio/src/pages/about.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const about = () => {
+    return (
+        <>
+        <section className="portfolio-section" id="about-me">
+            <div className="section-title-container">
+                <h2 className="section-title">About Me</h2>
+            </div>
+            <div id="about-me-summary">
+                <p>
+
+                </p>
+            </div>
+            <div id="skills">
+
+            </div>
+        </section>
+        </>
+    )
+}
