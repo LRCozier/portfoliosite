ac91a534fb1875c5457799f1d27bf571b120f823
Luke Rudderham-Cozier <lrcozier@gmail.com>
2024-07-21 19:48:45 +0100

    created home.jsx

---
+++ LRCozierportfolio/src/pages/home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const home = () => {
+
+    return(
+        <>
+        <section className="portfolio-section" id="hero">
+            <h1>Luke Rudderham-Cozier</h1>
+            <h2>Front End Developer</h2>
+        </section>
+        </>
+    )
+}
+
+export default home;
