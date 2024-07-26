5cfbf36f06795360bc29c7a144986e8701e8bea4
Luke Rudderham-Cozier <lrcozier@gmail.com>
2024-07-26 19:34:55 +0100

    created projects.jsx and exported to app.js

--- LRCozierportfolio/src/App.jsx
+++ LRCozierportfolio/src/App.jsx
@@ -4,6 +4,7 @@ import Header from './Components/Header/header';
 import Footer from './Components/Footer/footer';
 import Home from './pages/home';
 import About from './pages/about';
+import ProjectsDisplay from './pages/projects';
 import Contact from './pages/contact';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
     <Header />
     <Home />
     <About/>
+    <ProjectsDisplay/>
     <Contact />
     <Footer />
     </div>
---
+++ LRCozierportfolio/src/pages/projects.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const ProjectsDisplay = () => {
+
+    return(
+        <div className="projects">
+            <section className="portfolio-section">
+                <h2>Projects</h2>
+                <div className="project-display-container"></div>
+                <div className="project-display-container"></div>
+                <div className="project-display-container"></div>
+                <div className="project-display-container"></div>
+            </section>
+
+        </div>
+    )
+}
+
+export default ProjectsDisplay;
