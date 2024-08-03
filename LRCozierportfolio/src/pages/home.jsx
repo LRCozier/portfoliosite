fb725ea7f435543b91af26800483ac704950b42e
Luke Rudderham-Cozier <lrcozier@gmail.com>
2024-08-03 17:57:20 +0100

    imported profile pic to hero section

--- LRCozierportfolio/src/pages/home.jsx
+++ LRCozierportfolio/src/pages/home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import profile from '..assets/images/profile.jpg';
 
 const Home = () => {
 
     return(
         <div className="hero" id="hero">
         <section className="portfolio-section" id="hero-section">
+            <img src={profile} alt="profilepicture"></img>
             <h1>Luke Rudderham-Cozier</h1>
             <h2>Front End Developer</h2>
         </section>
